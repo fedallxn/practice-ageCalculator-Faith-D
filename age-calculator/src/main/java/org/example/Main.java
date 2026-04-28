ab89956cccc547bc0f0eca1f94a9598b5e7a0b37
Faith <faithe.dall@gmail.com>
2026-04-28 13:06:26 -0500

    Created an age calculator that prompts the user for their age and returns a string that says how many years they have left until they turn 100 years old.

---
+++ age-calculator/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //prompts for the user to enter their age
+        System.out.print("Enter your age: ");
+        //storing as int instead of a double because not expecting decimals
+        int age = scanner.nextByte();
+
+        int difference = 100 - age;
+        System.out.print("You are " + difference + " years away from being 100 years old!");
+    }
+}
